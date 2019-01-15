d553da61b93ae4dfe854397e8bf974a81f16c9a2
ersaurav03 <er.saurav03@gmail.com>
2019-01-15 18:35:58 +0530

    added graphs with components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,26 +1,19 @@
 
 <template>
-<monthly-income :data="test" />
+<monthly-income :height="300" />
 </template>
 <script>
+  import { Line } from 'vue-chartjs';
 import MonthlyIncome from './monthlyincome.vue'
 export default {
-data:function(){
-return{
-test:{
-labels: ['January', 'February'],
-datasets: [
-{
-label: 'Data One',
-backgroundColor: '#f87979',
-data: [40, 20]
-}
-]
-}
-}
- 
-},
+
 components: { MonthlyIncome },
+
 }
+
+
 </script>
+<style>
+  .d1{}
+</style>
 
--- src/components/monthlyincome.vue
+++ src/components/monthlyincome.vue
@@ -1,11 +1,23 @@
-
 <script>
 import { Bar } from 'vue-chartjs';
 export default {
 extends: Bar,
 props: ['data', 'options'],
 mounted () {
-this.renderChart(this.data, this.options)
+this.renderChart({
+labels: ['Jan', 'Feb','Mar','Apr','May',"Jun"],
+datasets: [
+{
+label: 'Data One',
+backgroundColor: '#f87979',
+data: [10,5,30,15,50,20]
+}
+]
+}, {
+	responsive:false,
+	maintainAspectRatio:false
+
+})
 }
 }
 </script>
