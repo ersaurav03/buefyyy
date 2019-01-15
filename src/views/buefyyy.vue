7a0c4559a011bbed839c04cf82bfb9165c48dbd2
ersaurav03 <er.saurav03@gmail.com>
2019-01-15 07:03:18 +0530

    till 32.27

---
+++ src/views/buefyyy.vue
@@ -0,0 +1,396 @@
+<template>
+	<!-- HEadings-->
+	<div class="">
+		<div class="block">
+			<!-- <h1 >JSMPJ Corporation</h1> -->
+			<h1 class="title">This is h1 with title</h1>
+			<h2 class="subtitle">This is h2 with subtitle</h2>
+
+
+			<!-- Sizes -->
+			<h1 class="title is-1">Hello World</h1>
+			<h2 class="title is-2">Hello World</h2>
+			<h3 class="title is-3">Hello World</h3>
+			<h4 class="title is-4">Hello World</h4>
+			<h5 class="title is-5">Hello World</h5>
+			<h6 class="title is-6">Hello World</h6>
+		</div>
+
+
+		<!-- Contents-->
+		<div class="block">
+			<p class="content is-small">Lightweight UI components for Vue.js based on Bulma - buefy/buefy. ... href="//cdn.materialdesignicons.com/2.0.46/css/materialdesignicons.min.css">.
+				‎            Buefy · ‎Issues 145 · ‎31 releases · ‎458
+			</p>
+
+			<p class=" content is-medium">Lightweight UI components for Vue.js based on Bulma - buefy/buefy. ... href="//cdn.materialdesignicons.com/2.0.46/css/materialdesignicons.min.css">.
+				‎            Buefy · ‎Issues 145 · ‎31 releases · ‎458
+			</p>
+
+			<p class="content is-large">Lightweight UI components for Vue.js based on Bulma - buefy/buefy. ... href="//cdn.materialdesignicons.com/2.0.46/css/materialdesignicons.min.css">.
+				‎            Buefy · ‎Issues 145 · ‎31 releases · ‎458
+			</p>
+		</div>
+
+		<!-- Buttons -->
+		<div class="block">
+			<button class="button">Button</button>
+			<button class="button is-white">Button</button>
+			<button class="button is-light">Button</button>
+			<button class="button is-dark">Button</button>
+			<button class="button is-black">Button</button>
+			<button class="button is-info">Button</button>
+			<button class="button is-link">Linkk</button>
+		</div>
+
+		<!-- color classes in button -->
+		<div class="block">
+			<a href="" class="button is-primary">Primary</a>
+			<a href="" class="button is-info">Info</a>
+			<a href="" class="button is-success">success</a>
+			<a href="" class="button is-warning">Warning</a>
+			<a href="" class="button is-Danger">Danger</a>
+			<a href="" class="button is-light">Light</a>
+			<a href="" class="button is-dark">Dark</a>
+		</div>
+
+
+		<!-- color classes in button sizes-->
+		<div class="block">
+			<a href="" class="button is-primary is-small">Primary</a>
+			<a href="" class="button is-info is-medium">Info</a>
+			<a href="" class="button is-success is-large">success</a>
+
+		</div>
+
+		<!-- color classes in button outlines-->
+		<div class="block">
+			<a href="" class="button is-primary is-outlined">Primary</a>
+			<a href="" class="button is-info is-outlined">Info</a>
+			<a href="" class="button is-success is-outlined">success</a>
+			<a href="" class="button is-warning is-outlined">Warning</a>
+			<a href="" class="button is-Danger is-outlined">Danger</a>
+			<a href="" class="button is-light is-outlined">Light</a>
+			<a href="" class="button is-dark is-outlined">Dark</a>
+		</div>
+
+		<!-- color classes in button is inverted-->
+		<div class="block">
+			<a href="" class="button is-primary is-inverted">Primary</a>
+			<a href="" class="button is-info is-inverted">Info</a>
+			<a href="" class="button is-success is-inverted">success</a>
+			<a href="" class="button is-warning is-inverted">Warning</a>
+			<a href="" class="button is-Danger is-inverted">Danger</a>
+			<a href="" class="button is-light is-inverted">Light</a>
+			<a href="" class="button is-dark is-inverted">Dark</a>
+		</div>
+		<!-- Button states-->
+		<div class="block">
+			<a href="" class="button is-hovered">Hovered</a>
+			<a href="" class="button is-focused">Focused</a>
+			<a href="" class="button is-active">Active</a>
+			<a href="" class="button is-loading">Loading</a>
+			<a href="" class="button " disabled>Disabled</a>
+		</div>
+
+		<!-- Boxx -->
+		<div class="block">
+			<div class="box">
+				<h1 class="title">Heading of Box</h1>
+				<p>thsis is all about box classs we have to do it</p>
+			</div>
+		</div>
+
+		<!-- Notifications -->
+		<div class="block">
+			<div class="notification">	
+				<button class="delete"></button>
+				This is all about notifiactions we hav to do it
+			</div>
+
+			<div class="notification is-primary">	
+				<button class="delete"></button>
+				This is all about notifiactions we hav to do it
+			</div>
+
+			<div class="notification is-info">	
+				<button class="delete"></button>
+				This is all about notifiactions we hav to do it
+			</div>
+
+			<div class="notification is-success">	
+				<button class="delete"></button>
+				This is all about notifiactions we hav to do it
+			</div>
+
+			<div class="notification is-danger">	
+				<button class="delete"></button>
+				This is all about notifiactions we hav to do it
+			</div>
+
+			<div class="notification is-warning">	
+				<button class="delete"></button>
+				This is all about notifiactions we hav to do it
+			</div>
+		</div>
+
+		<!-- tagsss -->
+		<div class="block">
+			<span class="tag">tagg</span>
+			<span class="tag is-black">Black</span>
+			<span class="tag is-light">Light</span>
+			<span class="tag is-white">white</span>
+			<span class="tag is-primary">primary</span>
+			<span class="tag is-info">info</span>
+			<span class="tag is-warning">warning</span>
+			<span class="tag is-success">success</span>
+			<span class="tag is-danger is-medium">danger</span>
+			<span class="tag is-dark is-large">dark<button class="delete"></button></span>
+		</div>
+
+		<!-- Messages -->
+		<div class="block">
+			<article class="message">
+				<div class="message-header">
+					<p>About Us</p>
+				</div>
+				<div class="message-body">
+					This is message body
+					heree you can write message
+				</div>
+			</article>
+
+			<article class="message is-primary">
+				<div class="message-header">
+					<p>About Us</p>
+				</div>
+				<div class="message-body">
+					This is message body
+					heree you can write message
+				</div>
+			</article>
+
+			<article class="message is-success">
+				<div class="message-header">
+					<p>About Us</p>
+				</div>
+				<div class="message-body">
+					This is message body
+					heree you can write message
+				</div>
+			</article>
+
+			<article class="message is-info">
+				<div class="message-header">
+					<p>About Us</p>
+				</div>
+				<div class="message-body">
+					This is message body
+					heree you can write message
+				</div>
+			</article>
+
+			<article class="message is-warning">
+				<div class="message-header">
+					<p>About Us</p>
+				</div>
+				<div class="message-body">
+					This is message body
+					heree you can write message
+				</div>
+			</article>
+
+			<article class="message is-danger">
+				<div class="message-header">
+					<p>About Us</p>
+				</div>
+				<div class="message-body">
+					This is message body
+					heree you can write message
+				</div>
+			</article>
+		</div>
+		<!-- Navvbar -->
+		<div class="block">
+			<nav class="nav">
+
+				<div class="nav-left is-active">
+					<a href="" class="nav-item">
+						<h1 class="title is-4">Left navbar</h1>
+					</a>
+				</div>
+
+				<div class="nav-center">
+					<a href="" class="nav-item">
+						<span class="icon">
+							<i class="fab fa-twitter"></i>
+						</span>
+					</a>
+
+					<a href="" class="nav-item">
+						<span class="icon">
+							<i class="fab fa-twitter"></i>
+						</span>
+					</a>
+
+					<a href="" class="nav-item">
+						<span class="icon">
+							<i class="fab fa-github"></i>
+						</span>
+					</a>
+				</div>
+
+				<div class="nav-right nav-menu">
+					<a href="" class="nav-item">Home</a>
+					<a href="" class="nav-item">About</a>
+					<a href="" class="nav-item">Contacts</a>
+					<a href="" class="nav-item">Account</a>
+				</div>
+			</nav>
+		</div>
+<div class="block">
+	<aside class="menu">
+		<p class="menu-label">
+			Label 1
+		</p>
+		<ul class="menu-list">
+			<li><a href="">Link 1</a></li>
+			<li><a href="">Link 2</a></li>
+			<li><a href="">Link 3</a></li>
+		</ul>
+
+		<p class="menu-label">
+			Label 2
+		</p>
+		<ul class="menu-list">
+			<li><a href="">Link 1</a></li>
+			<li><a href="" class="is-active">Link 2</a>
+			<ul>
+			<li><a href="">Link 2.1</a></li>
+			<li><a href="">Link 2.2</a></li>
+			<li><a href="">Link 2.3</a></li>
+			</ul>
+			</li>
+            <li><a href="">Link 2</a></li>
+			<li><a href="">Link 3</a></li>
+		</ul>
+	</aside>
+</div>
+
+        <!-- Heroo -->
+         <div class="block">
+         	 <section class="hero">
+         		<div class="hero-body">
+         			<div class="container">
+         				<h1 class="title">Hero title</h1>
+         				<h1 class="subtitle">Hero Sub title</h1>
+         			</div>
+         		</div>
+         	</section>
+         	<section class="hero is-primary">
+         		<div class="hero-body">
+         			<div class="container">
+         				<h1 class="title">Hero title</h1>
+         				<h1 class="subtitle">Hero Sub title</h1>
+         			</div>
+         		</div>
+         	</section>
+               <section class="hero is-danger">
+         		<div class="hero-body">
+         			<div class="container">
+         				<h1 class="title">Hero title</h1>
+         				<h1 class="subtitle">Hero Sub title</h1>
+         			</div>
+         		</div>
+         	</section>
+
+         	<section class="hero is-info">
+         		<div class="hero-body">
+         			<div class="container">
+         				<h1 class="title">Hero title</h1>
+         				<h1 class="subtitle">Hero Sub title</h1>
+         			</div>
+         		</div>
+         	</section>
+
+         	<section class="hero is-warning">
+         		<div class="hero-body">
+         			<div class="container">
+         				<h1 class="title">Hero title</h1>
+         				<h1 class="subtitle">Hero Sub title</h1>
+         			</div>
+         		</div>
+         	</section>
+
+         	<section class="hero is-dark">
+         		<div class="hero-body">
+         			<div class="container">
+         				<h1 class="title">Hero title</h1>
+         				<h1 class="subtitle">Hero Sub title</h1>
+         			</div>
+         		</div>
+         	</section>
+
+         	 <section class="hero is-success">
+         		<div class="hero-body">
+         			<div class="container">
+         				<h1 class="title">Hero title</h1>
+         				<h1 class="subtitle">Hero Sub title</h1>
+         			</div>
+         		</div>
+         	</section>
+
+         </div>
+<!-- Cards -->
+     <div class="block">
+     	<div class="card">
+     		<div class="card-content">
+     			<p class="title">
+     				This is all about
+     			</p>
+     		</div>
+     		<footer class="card-footer">
+     			<p class="card-footer-item">
+     				<span>
+     					View on <a href=""><i class="fab fa-twitter"></i>Twitter</a>
+     				</span>
+     			</p>	
+
+     			<p class="card-footer-item">
+     				<span>
+     					View on <a href=""><i class="fab fa-facebook"></i>Facebook</a>
+     				</span>
+     			</p>	
+     		</footer>
+     	</div>
+     </div>
+
+     <!-- Pagination -->
+     <div class="block">
+     	<nav class="pagination">
+     		<a href="" class="pagination-previous" disabled>Previous</a>
+     		<a href="" class="pagination-previous" >Next</a>
+     		<ul class="pagination-list">
+     			<li>
+     				<a href="" class="pagination-link is-current">1</a>
+     			</li>
+     			<li>
+     				<a href="" class="pagination-link">2</a>
+     			</li>
+     			<li>
+     				<a href="" class="pagination-link">3</a>
+
+     			</li>
+     		</ul>
+     	</nav>
+     </div>
+		<div style="margin-top:500px"></div>
+	</div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+</style>
