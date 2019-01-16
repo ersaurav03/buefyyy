d588f180268d39834a5a898a713b03f9c217041b
ersaurav03 <er.saurav03@gmail.com>
2019-01-17 00:04:29 +0530

    added apiu link

--- src/views/axxios.vue
+++ src/views/axxios.vue
@@ -1,23 +1,84 @@
 <template>
-	<div>
-		<h1>JSMPJ quotes comes from: {{info}}</h1>
-	</div>
+  <div>
+    <h1>JSMPJ quotes comes from: {{info}}</h1>
+
+<!--       <h1>Bitcoin Price Index</h1>
+  <div
+    v-for="currency in info"
+    class="currency"
+  >
+    {{ currency.description }}:
+    <span class="lighten">
+      <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
+    </span>
+  </div> -->
+
+    <!-- <h1>Bitcoin Price Index</h1>
+
+  <section v-if="errored">
+    <p>We're sorry, we're not able to retrieve this information at the moment, please try back later</p>
+  </section>
+
+  <section v-else>
+    <div v-if="loading">Loading...</div>
+
+    <div
+      v-else
+      v-for="currency in info"
+      class="currency"
+    >
+      {{ currency.description }}:
+      <span class="lighten">
+        <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
+      </span>
+    </div>
+</section> -->
+  </div>
 </template>
 
 <script>
-	 import axios from 'axios'
-	export default{
-  
-       data () {
+import axios from 'axios'
+export default{
+
+  // data () {
+  //   return {
+  //     info: null,
+  //     loading: true,
+  //     errored: false
+  //   }
+  // },
+  // filters: {
+  //   currencydecimal (value) {
+  //     return value.toFixed(2)
+  //   }
+  // },
+  // mounted () {
+  //   axios
+  //     .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+  //     .then(response => {
+  //       this.info = response.data.bpi
+  //     })
+  //     .catch(error => {
+  //       console.log(error)
+  //       this.errored = true
+  //     })
+  //     .finally(() => this.loading = false)
+  // }
+  data () {
     return {
       info: ''
     }
   },
+    filters: {
+  currencydecimal (value) {
+    return value.toFixed(2)
+  }
+},
   mounted () {
     axios
-      .get('https://api.myjson.com/bins/w5ni4')
-      .then(response => (this.info = response))
+    .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(response => (this.info = response.data.bpi))
   }
-	}
+}
 
 </script>
