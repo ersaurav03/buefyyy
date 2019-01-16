e750702449229e487f0da70745179e0c16fd1cd4
ersaurav03 <er.saurav03@gmail.com>
2019-01-17 00:20:37 +0530

    added apiu link

--- src/views/axxios.vue
+++ src/views/axxios.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-<!--     <h1>JSMPJ quotes comes from: {{info}}</h1>
+<h1>Part 1</h1>
+
+<h6>{{info}}</h6>
+
+
+<!--  <h1> Part 2</h1>
+
+
+  <h1>JSMPJ quotes comes from: {{info}}</h1>
 
       <h1>Bitcoin Price Index</h1>
   <div
@@ -13,7 +21,13 @@
     </span>
   </div> -->
 
-    <h1>Bitcoin Price Index</h1>
+
+
+
+
+<!--  <h1>  part3</h1>
+
+ <h1>Bitcoin Price Index</h1>
 
   <section v-if="errored">
     <p>We're sorry, we're not able to retrieve this information at the moment, please try back later</p>
@@ -32,38 +46,53 @@
         <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
       </span>
     </div>
-</section>
+</section> -->
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 export default{
-
+//part1
   data () {
     return {
-      info: null,
-      loading: true,
-      errored: false
-    }
-  },
-  filters: {
-    currencydecimal (value) {
-      return value.toFixed(2)
+      info: null
     }
   },
   mounted () {
     axios
       .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => {
-        this.info = response.data.bpi
-      })
-      .catch(error => {
-        console.log(error)
-        this.errored = true
-      })
-      .finally(() => this.loading = false)
+      .then(response => (this.info = response))
   }
+
+
+//Part 3
+  // data () {
+  //   return {
+  //     info: null,
+  //     loading: true,
+  //     errored: false
+  //   }
+  // },
+  // filters: {
+  //   currencydecimal (value) {
+  //     return value.toFixed(2)
+  //   }
+  // },
+  // mounted () {
+  //   axios
+  //     .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+  //     .then(response => {
+  //       this.info = response.data.bpi
+  //     })
+  //     .catch(error => {
+  //       console.log(error)
+  //       this.errored = true
+  //     })
+  //     .finally(() => this.loading = false)
+  // }
+
+  //part2 
 //   data () {
 //     return {
 //       info: ''
