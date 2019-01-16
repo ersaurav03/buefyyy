cdfb2c27fc59affe913c1e0d3674e0d69f035cff
ersaurav03 <er.saurav03@gmail.com>
2019-01-16 12:29:58 +0530

    Changes in mixx graphh

--- src/components/mix.vue
+++ src/components/mix.vue
@@ -1,43 +1,39 @@
 <script>
-import { Bar } from 'vue-chartjs';
+import { Line } from 'vue-chartjs';
 export default {
-extends: Bar,
-props: ['data', 'options'],
+extends: Line,
+// props: ['data', 'options'],
 mounted () {
 this.renderChart({
-  datasets: [{
-          label: 'Bar Dataset',
-          data: [10, 6, 30, 20,40,25],
-          backgroundColor:'rgba(255, 99, 132, 1.0)',           
-// borderColor: 'rgb(255, 0, 132)'
-        }, {
-          label: 'Line Dataset',
-          data: [20,100,30,15,32,11],
-backgroundColor:'rgba(0, 99, 132, 0.2)',           
-// borderColor: 'rgb(255, 0, 132)',
-          // Changes this dataset to become a line
-          type: 'line'
-        }],
-    labels: ['January', 'February', 'March', 'April']
+datasets: [{
+label: 'FIS',
+data: [10, 6, 30, 20,40,40],
+// backgroundColor:'rgba(255, 99, 132, 0.5)',           
+borderColor: 'rgb(0, 255, 0)'
+},
+{
+label: 'EVRY',
+data: [20,0,30,15,32,50],
+// backgroundColor:'rgba(0, 99, 132, 0.5)',           
+borderColor: 'rgb(255, 0, 0)',
+// Changes this dataset to become a line
+// type: 'line'
+},
+{
+label: 'Maropost',
+data: [10,20,30,40,50,55],
+// backgroundColor:'rgba(0, 99, 132, 0.5)',           
+borderColor: 'rgb(0, 0, 255)',
+// Changes this dataset to become a line
+// type: 'line'
+},
+
+],
+labels: ['January', 'February', 'March', 'April','May','June']
 }, {
-	responsive:false,
-	maintainAspectRatio:false
+responsive:false,
+maintainAspectRatio:false
 })
 }
 }
 </script>
-<!-- 
-<style type="text/css">
-  
-  datasets: [{
-          label: 'Bar Dataset',
-          data: [10, 20, 30, 40]
-        }, {
-          label: 'Line Dataset',
-          data: [50, 50, 50, 50],
-
-          // Changes this dataset to become a line
-          type: 'line'
-        }],
-    labels: ['January', 'February', 'March', 'April']
-</style> -->
